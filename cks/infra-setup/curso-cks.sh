a4c1e794a824e9abe89d9d7950e69821ec540730
4Linux <67426067+curso-4linux@users.noreply.github.com>
2023-08-28 16:33:39 -0300

    Update curso-cks.sh

--- cks/infra-setup/curso-cks.sh
+++ cks/infra-setup/curso-cks.sh
@@ -7,11 +7,11 @@ sudo snap install helm --classic
 #### Metallb Install
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install metallb bitnami/metallb
-kubectl apply -f /home/suporte/4541/cks/infra-setup/metallb-config.yaml
+kubectl apply -f /home/suporte/545/cks/infra-setup/metallb-config.yaml
 
 #### Nginx Ingress Install
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm install ingress-nginx ingress-nginx/ingress-nginx --version 3.34.0
 
 ### Deploy PHP 4Linux
-kubectl apply -f /home/suporte/4541/cks/infra-setup/recursos.yaml
+kubectl apply -f /home/suporte/545/cks/infra-setup/recursos.yaml
